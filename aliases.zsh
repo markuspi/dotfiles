3f1a7267741566da338c7871fdac07fd1af383f1
Markus <markus.pielmeier@tum.de>
2019-07-15 22:19:39 +0200

    New aliases, move functions to separate file

--- aliases.zsh
+++ aliases.zsh
@@ -5,7 +5,10 @@ alias inst="sudo apt install"
 alias sinst="sudo snap install"
 alias alig="apt list --installed | grep"
 alias myip="hostname -I"
-alias hex="python3 -c 'import sys; print(hex(int(sys.argv[1])))'"
+alias hex="python3 -c 'import sys; print(hex(int(sys.argv[1])))' --"
+alias tmpd="cd $(mktemp -d)"
+alias x="xdg-open"
+alias apk-search="adb shell pm list packages | grep"
 
 # edit common config files
 alias ed-al="vim ~/.dotfiles/aliases.zsh && . ~/.dotfiles/aliases.zsh"
@@ -14,13 +17,5 @@ alias ed-ssh="vim ~/.ssh/config"
 alias ed-zsh="vim ~/.zshrc"
 
 alias dot-pull="git -C ~/.dotfiles pull && ~/.dotfiles/install.zsh"
-
-function venv {
-    if [ -d "venv" ]; then
-        . venv/bin/activate
-    else
-        . .venv/bin/activate
-    fi
-}
 
 # vi: ft=zsh
---
+++ functions.zsh
@@ -0,0 +1,35 @@
+
+function venv {
+    if [ -d "venv" ]; then
+        . venv/bin/activate
+    else
+        . .venv/bin/activate
+    fi
+}
+
+function mkcd {
+    if [ "$#" -ne "1" ]; then
+        echo "Usage: $0 <directory>"
+        return -1
+    fi
+
+    mkdir -p "$1"
+    cd "$1"
+}
+
+function apk-pull {
+    if [ "$#" -ne "1" ]; then
+        echo "Usage: $0 <search-term>"
+        return -1
+    fi
+
+    search=$(apk-search "$1")
+    results=$(echo $search | wc -l)
+    if [ "$results" -ne "1" ]; then
+        echo "Need exactly 1 result, got $results"
+        return -1
+    fi
+    package="${search#*:}"
+    packpath=$(adb shell pm path "$package")
+    adb pull "${packpath#*:}"
+}
