6dc4f8cb8bbcce5d48a97212349a48f3cd5ce83e
Markus Pielmeier <markuspi@users.noreply.github.com>
2020-08-05 13:38:30 +0200

    Autosave (2020-08-05 13:38:30)

--- install.zsh
+++ install.zsh
@@ -11,24 +11,29 @@ if [[ "$HOME" != "/home"* ]]; then
     esac
 fi
 
-for file in "$HOME"/.dotfiles/home/*; do
-    name="${file:t}"
-    linkname="$HOME/.$name"
+function make_links {
+    for file in "$1"/*; do
+        name="${file:t}"
+        linkname="$2/$name"
 
-    # check if there is a non-link file that would be overwritten
-    if [ -e "$linkname" ] && [ ! -L "$linkname" ]; then
-        echo "Backing up: $linkname >> $linkname.bak"
-        mv "$linkname" "$linkname.bak"
-    fi
+        # check if there is a non-link file that would be overwritten
+        if [ -e "$linkname" ] && [ ! -L "$linkname" ]; then
+            echo "Backing up: $linkname >> $linkname.bak"
+            mv "$linkname" "$linkname.bak"
+        fi
 
-    if [ -L "$linkname" ]; then
-        echo "Deleting old link $linkname"
-        rm "$linkname"
-    fi
+        if [ -L "$linkname" ]; then
+            echo "Deleting old link $linkname"
+            rm "$linkname"
+        fi
 
-    echo "Linking $linkname -> $file"
-    ln -s "$file" "$linkname"
-done
+        echo "Linking $linkname -> $file"
+        ln -s "$file" "$linkname"
+    done
+}
+
+make_links "$HOME/.dotfiles/home" "$HOME"
+make_links "$HOME/.dotfiles/bin" "$HOME/.local/bin"
 
 # install useful python packages
 pip3 install --upgrade --user tldr bpython virtualenv
